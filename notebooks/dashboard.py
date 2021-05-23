6650da23835c248cb823b3d47d76f49682c22681
sepehrfrgh <seforoug@ucsd.edu>
2021-05-22 19:19:36 -0700

    table represting

--- notebooks/dashboard.py
+++ notebooks/dashboard.py
@@ -4,7 +4,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
 from dash.dependencies import Input, Output, State
-
 df = pd.read_csv('../data/filtered_books.csv', error_bad_lines = False)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -32,28 +31,46 @@ layout_tab_2 = html.Div(children=[
     html.Br(),
     html.Div(["Search Your Book By Authors: ", dcc.Input(id='my-input', value='', type='text')]),
     html.Br(),
-
     html.Div(id='my-output'),
-
 ])
-test = html.Div(children=[dash_table.DataTable(
-    id='table-filtering',
-    columns=[
-        {"name": i, "id": i} for i in sorted(df.columns)
-    ],
-    page_current=0,
-    page_size=12,
-    page_action='custom',
 
-    filter_action='custom',
-    filter_query=''
-)])
 @app.callback(
-    Output(component_id='table-filtering', component_property='children'),
+    Output(component_id='my-output', component_property='children'),
     Input(component_id='my-input', component_property='value'),
 )
 def update_output_div(input_value):
-    return input_value
+    sep = title_contains_word(input_value)
+    return [
+        dash_table.DataTable(
+            data=sep.to_dict('records'),
+            columns=[{'id': c, 'name': c} for c in sep.columns],
+            style_data={
+                'width': '100px',
+                'maxWidth': '100px',
+                'minWidth': '100px',
+            },
+            style_cell_conditional=[
+                {
+                    'if': {'column_id': 'Region'},
+                    'width': '250px'
+                },
+            ],
+            style_table={
+                'overflowX': 'auto'
+            }
+        )
+    ]
+def title_contains_word(word):
+    """
+    input: desired word
+    output: books containing that word in their title
+    """
+    rows = []
+    for i in range(df.shape[0]):
+        if word in df['book_title'].iloc[i].decode('utf-8'):
+            rows.append(i)
+    filtered = df.iloc[list(rows)]
+    return filtered
 
 @app.callback(
     Output('tabs-content', 'children'),
@@ -68,31 +85,4 @@ def render_content(tab):
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-def title_contains_word(word):
-    """
-    input: desired word
-    output: books containing that word in their title
-    """
-    assert isinstance(word, str)
-    rows = []
-    for i in range(books.shape[0]):
-        if word in books['book_title'].iloc[i]:
-            rows.append(i)
-    filtered = books.iloc[list(rows)]
 
-    return filtered
-
-def book_authors(input_string):
-    """
-    input: title
-    output: book authors
-    """
-    if title_contains_word(input_string).shape[0]==1:
-        title = title_contains_word(input_string).iloc[0]['book_title']
-        authors = ""
-        for i in range(books.shape[0]):
-            if title in books['book_title'].iloc[i]:
-                authors = books['book_authors'].iloc[i]
-    else:
-        return "Here the user can choose between the books which are shown or we can force them to be more specific" #future
-    return [_.capitalize() for _ in authors.split("|")]
